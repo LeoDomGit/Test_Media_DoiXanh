6f062f1ca4ae182833ed63842f3e32d1912cb92c
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-03-19 19:34:50 +0700

    Update UserImport.php

--- Project/api/app/Imports/UserImport.php
+++ Project/api/app/Imports/UserImport.php
@@ -6,23 +6,6 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToArray;
 class UserImport implements ToArray
 {
-    // public function collection(Collection $rows)
-    // {
-    //     foreach ($rows as $key=> $row) {
-    //         // Skip header row or any other rows that need to be skipped
-    //         if ($key==0 || $key==1 || $row->isEmpty()) {
-    //             continue;
-    //         }
-    //         $data = [
-    //             'group_id' => $row[1],
-    //             'first_name'=> $row[2],
-    //             'last_name'=> $row[3],
-    //             'email'=> $row[4],
-    //             'phone'=> $row[5],
-    //         ];
-    //         User::create($data);
-    //     }
-    // }
      public function array(array $array)
     {
         return $array;
